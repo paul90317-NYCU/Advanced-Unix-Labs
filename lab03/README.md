7d92a0153a6a8fb2a1058952fdd5fd3c36bcad02
paul90317 <paul2309825@gmail.com>
2024-04-11 04:32:47 +0000

    Finish DFS and hijack the functions

--- lab03/README.md
+++ lab03/README.md
@@ -1,3 +1,6 @@
+# lab03
+## maze.c
+```c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,4 +20,4 @@ int main() {
 	printf("\nNo no no ...\n");
 	return 0;
 }
-
+```
--- lab03/libmaze.c
+++ lab03/libmaze.c
@@ -23,6 +23,8 @@ maze_get_ptr() {
 	return __stored_ptr;
 }
 
+void DFS(maze_t *mz);
+
 maze_t *
 maze_load(const char *fn) {
 	maze_t *mz = NULL;
@@ -55,6 +57,7 @@ maze_load(const char *fn) {
 	fclose(fp);
 	fprintf(stderr, "MAZE: loaded [%d, %d]: (%d, %d) -> (%d, %d)\n",
 		mz->w, mz->h, mz->sx, mz->sy, mz->ex, mz->ey);
+	DFS(mz);
 	return mz;
 err_quit:
 	if(mz) free(mz);
@@ -91,7 +94,58 @@ void move_left(maze_t *mz)   { move_dir(mz, 2); }
 void move_right(maze_t *mz)  { move_dir(mz, 3); }
 void move_random(maze_t *mz) { move_dir(mz, rand() % 4); }
 
-#define MOVE(n)	void move_##n(maze_t *mz) { move_random(mz); }
+int move_list[1200];
+int move_count;
+void perform_move(maze_t *mz, int i){
+    if(i >= move_count)
+        return;
+    move_dir(mz, move_list[i]);
+	printf("%d ", move_list[i]);
+    //printf("%d %d %d %d\n",mz->cx, mz->cy, end_x, end_y);
+    if(mz->cx == mz->ex && mz->cy == mz->ey){
+        puts("Bingo!");
+    }
+}
+
+
+// Depth-First Search 
+int finded;
+void dfs(maze_t *mz, int y, int x) {
+    if (move_count == 1200)
+        return;
+    if(finded)
+        return;
+    if (x == mz->ex && y == mz->ey) {
+        finded = 1;
+        return;
+    }
+    if(mz->blk[y][x])
+        return;
+
+    move_count += 1;
+    mz->blk[y][x] = 1;
+    for (int i = 0; i < 4; i++) {
+        int new_x = x + _dirx[i];
+        int new_y = y + _diry[i];
+        move_list[move_count - 1] = i;
+        dfs(mz, new_y, new_x);
+        if(finded){
+			mz->blk[y][x] = 0;
+			return;
+		}
+    }
+    mz->blk[y][x] = 0;
+    move_count -= 1;
+    return;
+}
+
+void DFS(maze_t *mz){
+	finded = 0;
+    move_count = 0;
+	dfs(mz, mz->sy, mz->sx); 
+}
+
+#define MOVE(n)	void move_##n(maze_t *mz) { perform_move(mz, n - 1); }
 #include "moves.c"
 #undef MOVE
 
